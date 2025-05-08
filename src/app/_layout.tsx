99d54762a5e25f6410b3d6cae99fb5d9bf657109
Renato Ferraz <renato.ferraz@qodeless.io>
2025-05-08 10:26:04 -0300

    Carregar fontes da aplicação

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,29 @@
 import { Slot } from "expo-router";
 import "@/styles/global.css";
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
+
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "@/components/Loading";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold,
+  });
   return (
     <View className="flex-1 bg-zinc-950 ">
-      <Slot />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Slot /> : <Loading />}
     </View>
   );
 }
